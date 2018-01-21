22086b213da1cff2e47b37fe0f4aa827085a9cc5
Stas D <stas.d@ak-tech.ca>
2018-01-21 10:32:26 +0200

    Internal object helpers

---
+++ src/Sd.Helpers.OData/Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Sd.Helpers.OData.InternalHelpers;
+
+namespace Sd.Helpers.OData
+{
+    public class Converter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static List<T> ToList<T>(IQueryable collection)
+        {
+            try
+            {
+                var resultList = new List<T>();
+
+                foreach (var res in collection)
+                {
+                    object instance = (T)Activator.CreateInstance(typeof(T));
+
+                    resultList.Add((T)instance);
+                }
+
+                return resultList;
+            }
+            catch (Exception ex)
+            {
+                return JsonConvert.DeserializeObject<List<T>>(JsonConvert.SerializeObject(collection)); // FallBack
+            }
+        }
+    }
+}
---
+++ src/Sd.Helpers.OData/InternalHelpers/ObjectHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sd.Helpers.OData.InternalHelpers
+{
+    internal class ObjectHelper
+    {
+        internal static object GetPropValue(object item, string propName)
+        {
+            if (item == null)
+                return null;
+
+            var prop = item.GetType().GetProperty(propName);
+
+            return prop != null ? prop.GetValue(item) : null;
+        }
+
+        internal static void SetPropValue(object item, string propName, object value)
+        {
+            var propertyInfo = item.GetType().GetProperty(propName);
+
+            if (propertyInfo != null)
+                propertyInfo.SetValue(item, ChangeType(value, propertyInfo.PropertyType), null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object ChangeType(object value, Type type)
+        {
+            switch (value)
+            {
+                case null when type.IsGenericType:
+                    return Activator.CreateInstance(type);
+                case null:
+                    return null;
+                default:
+                    break;
+            }
+
+            if (type == value.GetType()) return value;
+            if (type.IsEnum)
+            {
+                return value is string ? Enum.Parse(type, (string)value) : Enum.ToObject(type, value);
+            }
+
+            if (!type.IsInterface && type.IsGenericType)
+            {
+                var innerType = type.GetGenericArguments()[0];
+                var innerValue = ChangeType(value, innerType);
+                return Activator.CreateInstance(type, innerValue);
+            }
+
+            switch (value)
+            {
+                case string _ when type == typeof(Guid):
+                    return new Guid(value as string ?? throw new InvalidOperationException());
+                case string _ when type == typeof(Version):
+                    return new Version(value as string ?? throw new InvalidOperationException());
+            }
+
+            return !(value is IConvertible) ? value : Convert.ChangeType(value, type);
+        }
+    }
+}
